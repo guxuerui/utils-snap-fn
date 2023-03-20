9d592a79114ce02a4c2662d34043e33249d7350f
guxuerui <im.guxuerui@gmail.com>
2023-03-21 01:09:45 +0800

    feat(lib): build updated files

---
+++ lib/src/playground/index.d.ts
@@ -0,0 +1,3 @@
+import isMobile from './isMobile';
+import isSafari from './isSafari';
+export { isMobile, isSafari, };
---
+++ lib/src/playground/isMobile.d.ts
@@ -0,0 +1,1 @@
+export default function isMobile(v: string | number): boolean;

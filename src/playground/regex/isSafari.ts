b39f9dfc33fcf6aa7c785897ff824f81192a6d87
guxuerui <im.guxuerui@gmail.com>
2023-03-22 21:57:53 +0800

    docs(isSafari): add TSDoc comments

--- src/playground/regex/isSafari.ts
+++ src/playground/regex/isSafari.ts
@@ -1,3 +1,8 @@
+/**
+ * 判断是否为Safari浏览器
+ * @returns 返回布尔值，true表示是Safari浏览器，false表示不是
+ */
+
 export default function isSafari() {
   return /Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent)
 }

53ef184488d78daf81d7f22a3a91388c71994692
guxuerui <im.guxuerui@gmail.com>
2023-03-22 21:57:41 +0800

    docs(isMobile): add TSDoc comments

--- src/playground/regex/isMobile.ts
+++ src/playground/regex/isMobile.ts
@@ -1,3 +1,9 @@
+/**
+ * 判断输入的字符串或数字是否为中国大陆手机号码
+ * @param v - 待判断的字符串或数字
+ * @returns 返回布尔值，true表示是中国大陆手机号码，false表示不是
+ */
+
 export default function isMobile(v: string | number): boolean {
   return /^1[0-9][0-9]\d{8}$/.test(`${v}`)
 }

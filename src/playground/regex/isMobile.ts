e1da61543957809462c950d6a09f24702be61f61
guxuerui <im.guxuerui@gmail.com>
2023-03-23 17:54:54 +0800

    🔧 chore(isMobile.ts): update regex to support more mobile phone number formats

--- src/playground/regex/isMobile.ts
+++ src/playground/regex/isMobile.ts
@@ -1,9 +1,9 @@
 /**
- * 判断输入的字符串或数字是否为中国大陆手机号码
+ * 判断输入的字符串或数字是否为手机号码
  * @param v - 待判断的字符串或数字
- * @returns 返回布尔值，true表示是中国大陆手机号码，false表示不是
+ * @returns 返回布尔值，true表示是手机号码，false表示不是
  */
 
 export default function isMobile(v: string | number): boolean {
-  return /^1[0-9][0-9]\d{8}$/.test(`${v}`)
+  return /^(\+?0?86\-?)?1[3456789]\d{9}$/.test(`${v}`)
 }

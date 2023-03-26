80ab7117bc9051061425cc6bf7d4ac77c40194c6
guxuerui <im.guxuerui@gmail.com>
2023-03-26 21:20:38 +0800

    ✨ feat(playground/index.ts): add isIdCard function to regexp exports

--- src/playground/index.ts
+++ src/playground/index.ts
@@ -1,8 +1,10 @@
 import isMobile from './regexp/isMobile'
 import isSafari from './regexp/isSafari'
 import isEmail from './regexp/isEmail'
+import isIdCard from './regexp/isIdCard'
 export {
   isMobile,
   isSafari,
   isEmail,
+  isIdCard,
 }

bfb80ce669fbdcb30643c8e29a3c5030a68e0c4e
guxuerui <im.guxuerui@gmail.com>
2023-03-26 22:56:14 +0800

    🚀 feat(playground/index.ts): add isIpv4 function to exports

--- src/playground/index.ts
+++ src/playground/index.ts
@@ -2,9 +2,12 @@ import isMobile from './regexp/isMobile'
 import isSafari from './regexp/isSafari'
 import isEmail from './regexp/isEmail'
 import isIdCard from './regexp/isIdCard'
+import isIpv4 from './regexp/isIpv4'
+
 export {
   isMobile,
   isSafari,
   isEmail,
   isIdCard,
+  isIpv4,
 }

c7931d143525ab6b0e5250f49964a58ebcdf58e7
guxuerui <im.guxuerui@gmail.com>
2023-03-26 23:22:45 +0800

    ✨ feat(playground/index.ts): add isIpv6 function to the exported functions list

--- src/playground/index.ts
+++ src/playground/index.ts
@@ -3,6 +3,7 @@ import isSafari from './regexp/isSafari'
 import isEmail from './regexp/isEmail'
 import isIdCard from './regexp/isIdCard'
 import isIpv4 from './regexp/isIpv4'
+import isIpv6 from './regexp/isIpv6'
 
 export {
   isMobile,
@@ -10,4 +11,5 @@ export {
   isEmail,
   isIdCard,
   isIpv4,
+  isIpv6,
 }

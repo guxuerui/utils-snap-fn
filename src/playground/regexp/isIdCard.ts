90f32688151d95072d29bb915eaf061e71183da1
guxuerui <im.guxuerui@gmail.com>
2023-03-26 23:22:20 +0800

    🔍 chore(isIdCard.ts): add return type to isIdCard function 🔍 chore(isIpv4.ts): add return type to isIpv4 function

--- src/playground/regexp/isIdCard.ts
+++ src/playground/regexp/isIdCard.ts
@@ -4,6 +4,6 @@
  * @returns 返回布尔值，true表示是身份证号码，false表示不是
  */
 
-export default function isIdCard(str: string | number) {
+export default function isIdCard(str: string | number): boolean {
   return /^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/.test(`${str}`)
 }
--- src/playground/regexp/isIpv4.ts
+++ src/playground/regexp/isIpv4.ts
@@ -4,6 +4,6 @@
  * @returns 返回布尔值，true表示是IPV4地址，false表示不是
  */
 
-export default function isIpv4(str: string) {
+export default function isIpv4(str: string): boolean {
   return /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(str)
 }

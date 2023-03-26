4263eb6394d9e8e567e078900e240758f68b94bf
guxuerui <im.guxuerui@gmail.com>
2023-03-26 22:56:29 +0800

    ✅ test(isIpv4.test.ts): add tests for isIpv4 function

---
+++ test/isIpv4.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, it } from 'vitest'
+import isIpv4 from '../src/playground/regexp/isIpv4'
+
+describe('isIpv4', () => {
+  it('should return true when given a valid IPV4 address', () => {
+    expect(isIpv4('0.0.0.0')).toBe(true)
+    expect(isIpv4('192.168.0.1')).toBe(true)
+    expect(isIpv4('255.255.255.255')).toBe(true)
+  })
+
+  it('should return false when given an invalid IPV4 address', () => {
+    expect(isIpv4('256.0.0.0')).toBe(false)
+    expect(isIpv4('192.168.0.')).toBe(false)
+    expect(isIpv4('192.168.0.1.2')).toBe(false)
+  })
+})

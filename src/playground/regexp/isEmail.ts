839b52f134de80df20d5fd8afb614cc2e6652c8e
guxuerui <im.guxuerui@gmail.com>
2023-03-26 22:55:59 +0800

    feat(regexp): new file isIpv4

--- src/playground/regexp/isEmail.ts
+++ src/playground/regexp/isEmail.ts
@@ -3,6 +3,7 @@
  * @param str 待测试的邮箱地址
  * @returns 返回布尔值，true表示是邮箱地址，false表示不是
  */
+
 export default function isEmail(str: string): boolean {
   return /^(?=.{1,254}$)[A-Za-z0-9._%+-]+@(?:[A-Za-z0-9-]+\.)+[A-Za-z]{2,}$/.test(str)
 }
---
+++ src/playground/regexp/isIpv4.ts
@@ -0,0 +1,9 @@
+/**
+ * 判断是否为IPV4地址
+ * @param str 待测试的IP地址
+ * @returns 返回布尔值，true表示是IPV4地址，false表示不是
+ */
+
+export default function isIpv4(str: string) {
+  return /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(str)
+}

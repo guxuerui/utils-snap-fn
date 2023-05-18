b22246270d5b2ed4b553980cff932bcb9b844a24
guxuerui <im.guxuerui@gmail.com>
2023-05-18 11:44:44 +0800

    chore(lib): rebuild lib files

--- lib/src/playground/array/isArrayEqual.d.ts
+++ lib/src/playground/array/isArrayEqual.d.ts
@@ -1,5 +1,5 @@
 /**
- * 判断两个数组是否相等
+ * 判断两个数组是否相等(数组内元素是否相同)
  * @param arr1 arr2 待比较的数组
  * @returns 返回布尔值，true表示相等，false表示不相等
  * @description 首先判断两个数组是否同一引用，如果是就直接返回 true; 如果其中一个数组是 null 或 undefined 就返回 false; 如果两个数组长度不同就返回 false; 最后遍历每个元素进行对比，如果任意一个元素不相等就返回 false ，否则返回 true
---
+++ lib/src/playground/array/removeDuplicatesObj.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Removes duplicate objects from an array based on a specified key.
+ * @param arr The array of objects to remove duplicates from.
+ * @param key The key to use as the basis for removing duplicates.
+ * @returns An array of unique objects based on the specified key.
+ */
+export default function removeDuplicatesObj(arr: any[], key: string): any[];

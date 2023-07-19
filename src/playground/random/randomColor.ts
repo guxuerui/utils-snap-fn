024c9f353f92048c8b450552db8734e699b71482
guxuerui <im.guxuerui@gmail.com>
2023-07-19 16:56:16 +0800

    🔀 refactor(randomColor.ts): refactor randomColor function to support different color types and opacity parameter

--- src/playground/random/randomColor.ts
+++ src/playground/random/randomColor.ts
@@ -2,7 +2,15 @@
  * @returns 随机生成的色值
  * @description 随机生成十六进制的颜色色值
  */
+import randomNum from './randomNum'
 
-export default function randomColor() {
-  return `#${(`00000${(Math.random() * 0x1000000 << 0).toString(16)}`).slice(-6)}`
+export default function randomColor(type?: string, opacity?: number) {
+  switch (type) {
+    case 'hex':
+      return `#${(`00000${(Math.random() * 0x1000000 << 0).toString(16)}`).slice(-6)}`
+    case 'rgb':
+      return `rgba(${randomNum(0, 255)}, ${randomNum(0, 255)}, ${randomNum(0, 255)}, ${opacity})`
+    default:
+      return ''
+  }
 }

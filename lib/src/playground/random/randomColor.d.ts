79341c41ee9298990f6a008417b5dde266d8f700
guxuerui <im.guxuerui@gmail.com>
2023-07-19 16:56:53 +0800

    chore(lib): update build files

--- lib/src/playground/random/randomColor.d.ts
+++ lib/src/playground/random/randomColor.d.ts
@@ -1,5 +1,1 @@
-/**
- * @returns 随机生成的色值
- * @description 随机生成十六进制的颜色色值
- */
-export default function randomColor(): string;
+export default function randomColor(type?: string, opacity?: number): string;

344139ea03bcd1db7364d1ebf36c0f5275b47b4b
guxuerui <im.guxuerui@gmail.com>
2023-07-19 16:56:23 +0800

    🐛 fix(generateRandomColor.test.ts): update test cases to pass an argument to randomColor function

--- test/random/generateRandomColor.test.ts
+++ test/random/generateRandomColor.test.ts
@@ -3,11 +3,11 @@ import randomColor from '../../src/playground/random/randomColor'
 
 describe('generateRandomColor', () => {
   it('should returns a string', () => {
-    expect(typeof randomColor() === 'string').toBe(true)
+    expect(typeof randomColor('hex') === 'string').toBe(true)
   })
 
   it('should returns a valid hex color', () => {
-    const color = randomColor()
+    const color = randomColor('hex')
     assert.match(color, /^#[0-9A-F]{6}$/i)
   })
 })

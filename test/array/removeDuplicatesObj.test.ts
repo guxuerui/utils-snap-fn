55a1af75639a4cb5b1146f4cbc9868cfc2cf2569
guxuerui <im.guxuerui@gmail.com>
2023-07-30 14:32:06 +0800

    test(removeDuplicatesObj.test.ts): add type annotations to input and expected variables for better code readability and maintainability test(removeDuplicatesObj.test.ts): update expected value in the test case where input is null or undefined to match the actual behavior

--- test/array/removeDuplicatesObj.test.ts
+++ test/array/removeDuplicatesObj.test.ts
@@ -2,15 +2,20 @@ import { describe, expect, it } from 'vitest'
 import isArrayEqual from '../../src/playground/array/isArrayEqual'
 import removeDuplicatesObj from '../../src/playground/array/removeDuplicatesObj'
 
+interface User {
+  id: number
+  name: string
+}
+
 describe('removeDuplicatesObj should remove duplicate objects based on the given key', () => {
-  const input = [
+  const input: User[] = [
     { id: 1, name: 'John' },
     { id: 2, name: 'Jane' },
     { id: 3, name: 'John' },
     { id: 4, name: 'Bob' },
     { id: 5, name: 'Jane' },
   ]
-  const expected = [
+  const expected: User[] = [
     { id: 1, name: 'John' },
     { id: 2, name: 'Jane' },
     { id: 4, name: 'Bob' },
@@ -21,15 +26,15 @@ describe('removeDuplicatesObj should remove duplicate objects based on the given
   })
 
   it('removeDuplicatesObj should return an empty array if the input is empty', () => {
-    const input = []
-    const expected = []
+    const input: User[] = []
+    const expected: User[] = []
     const actual = removeDuplicatesObj(input, 'name')
     expect(isArrayEqual(actual, expected)).toBe(true)
   })
 
   it('removeDuplicatesObj should return an empty array if the input is null or undefined', () => {
     const input = undefined
-    const expected = [
+    const expected: User[] = [
       { id: 1, name: 'John' },
       { id: 2, name: 'Jane' },
       { id: 4, name: 'Bob' },

592521d86275c279ccb7da78de867e0fa0d6e0e9
guxuerui <im.guxuerui@gmail.com>
2023-07-31 18:36:01 +0800

    refactor(randomColor.ts): change the signature of the randomColor function to require the type parameter and make the opacity parameter optional docs(randomColor.ts): update the JSDoc comments to reflect the changes in the function signature and provide more information about the parameters and return value

--- src/playground/random/randomColor.ts
+++ src/playground/random/randomColor.ts
@@ -1,10 +1,12 @@
 /**
  * @returns 随机生成的色值
- * @description 随机生成十六进制的颜色色值
- */
+ * @param type 生成颜色的类型
+ * @param opacity 透明度
+ * @description 随机生成十六进制的颜色色值或rgba颜色
+*/
 import randomNum from './randomNum'
 
-export default function randomColor(type?: string, opacity?: number) {
+export default function randomColor(type: string, opacity?: number) {
   switch (type) {
     case 'hex':
       return `#${(`00000${(Math.random() * 0x1000000 << 0).toString(16)}`).slice(-6)}`

4bc460cf6cb8b93e5b5e744767ab56bf36eb206d
guxuerui <im.guxuerui@gmail.com>
2023-08-10 11:12:35 +0800

    refactor(findTreeNode.ts): update function signature and add type definitions for tree nodes

--- src/playground/tree/findTreeNode.ts
+++ src/playground/tree/findTreeNode.ts
@@ -1,13 +1,20 @@
 /**
  * findTreeNode - A function that traverses an object tree to find a node that matches a given key and value.
  *
- * @param {Object} tree - The object tree to traverse.
- * @param {string} key - The key to look for within each node of the tree.
- * @param {string | number} value - The value to match against the key within each node of the tree.
- * @returns {Object | null} - The node that matches the key and value, or null if no match is found.
- */
+ * @param tree - The object tree to traverse.
+ * @param key - The key to look for within each node of the tree.
+ * @param value - The value to match against the key within each node of the tree.
+ * @returns The node that matches the key and value, or null if no match is found.
+*/
 
-export default function findTreeNode(tree: any, key: string, value: string | number): any {
+type TreeNode<T, U> = {
+  id: T
+  name: U
+  [key: string]: T | U | TreeNode<T, U>[]
+  children: TreeNode<T, U>[]
+}
+
+export default function findTreeNode(tree: TreeNode<number, string>, key: string, value: string | number): TreeNode<number, string> | null {
   if (tree[key] === value)
     return tree
 

f5a79ffe30a9fd997cb162b2a3087a9f7e6e96a7
guxuerui <im.guxuerui@gmail.com>
2023-08-10 18:44:45 +0800

    docs(scrollTo.ts): update JSDoc comments for scrollTo function docs(setScrollTop.ts): update JSDoc comments for setScrollTop function

--- src/playground/dom/setScrollTop.ts
+++ src/playground/dom/setScrollTop.ts
@@ -1,5 +1,8 @@
 /**
- * @param value 滚动的距离
+ * Sets the scroll top position of the window.
+ *
+ * @param value - The value to set the scroll top position to.
+ * @return This function does not return anything.
 */
 
 export default function setScrollTop(value: number): void {

d38c5e1440d9bbe1a762a6f23a29bbef2fd63b5a
guxuerui <im.guxuerui@gmail.com>
2024-05-27 17:12:32 +0800

    chore(index.d.ts): add formatTimeLength function import and export to improve code organization and maintainability

--- lib/src/playground/index.d.ts
+++ lib/src/playground/index.d.ts
@@ -21,4 +21,5 @@ import findAllNode from './tree/findAllNode';
 import getScrollTop from './dom/getScrollTop';
 import setScrollTop from './dom/setScrollTop';
 import scrollTo from './dom/scrollTo';
-export { isPhoneNum, isSafari, isMobile, isEmail, isIdCard, isIpv4, isIpv6, isValidUUID, isArrayEqual, removeDuplicatesObj, generateUUID, randomNum, randomColor, capitalsFirstLetter, uppercaseEveryWord, uppercaseEveryLetters, lowercaseEveryLetters, copyToClipboard, findTreeNode, findAllNode, getScrollTop, setScrollTop, scrollTo, };
+import formatTimeLength from './date/formatTimeLength';
+export { isPhoneNum, isSafari, isMobile, isEmail, isIdCard, isIpv4, isIpv6, isValidUUID, isArrayEqual, removeDuplicatesObj, generateUUID, randomNum, randomColor, capitalsFirstLetter, uppercaseEveryWord, uppercaseEveryLetters, lowercaseEveryLetters, copyToClipboard, findTreeNode, findAllNode, getScrollTop, setScrollTop, scrollTo, formatTimeLength, };

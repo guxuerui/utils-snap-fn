af9bff48bce102ee0217a40103257b769840e76b
guxuerui <im.guxuerui@gmail.com>
2024-05-27 14:52:06 +0800

    chore(lib): update build files

---
+++ lib/src/playground/date/formatTimeLength.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Format time length.
+ * @param timeLength The time length in seconds.
+ * @returns The formatted time length.
+ *
+*/
+export default function formatTimeLength(timeLength: number): string;
--- lib/src/playground/tree/findTreeNode.d.ts
+++ lib/src/playground/tree/findTreeNode.d.ts
@@ -1,9 +1,9 @@
 /**
  * findTreeNode - A function that traverses an object tree to find a node that matches a given key and value.
  *
- * @param {Object} tree - The object tree to traverse.
- * @param {string} key - The key to look for within each node of the tree.
- * @param {string | number} value - The value to match against the key within each node of the tree.
- * @returns {Object | null} - The node that matches the key and value, or null if no match is found.
- */
+ * @param tree - The object tree to traverse.
+ * @param key - The key to look for within each node of the tree.
+ * @param value - The value to match against the key within each node of the tree.
+ * @returns The node that matches the key and value, or null if no match is found.
+*/
 export default function findTreeNode(tree: any, key: string, value: string | number): any;
